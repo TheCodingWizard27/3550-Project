693ff103d3cd714d5fe8534bd50b0dd37f7c29f8
Raunak Upreti <raunakupretics26@gmail.com>
2025-02-16 10:03:06 -0600

    Function for generating a RSA Key added

--- project1.py
+++ project1.py
@@ -10,3 +10,13 @@ from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import JSONResponse
 from typing import Dict, List
 
+app = FastAPI() # Creating an instance of FastAPI() object and storing it in app
+
+key_store = {} # Dictionary used for storing keys
+
+KEY_EXPIRY_TIME = 600 # Key expiry time set for 10 minutes
+
+
+# Function for generating a RSA Key
+def generate_rsa_key():
+    
