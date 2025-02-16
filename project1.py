fa2e774183674cfd0ee4400952cc8a5fdca5fa05
Raunak Upreti <raunakupretics26@gmail.com>
2025-02-16 10:13:05 -0600

    Program generates RSA Key

--- project1.py
+++ project1.py
@@ -17,6 +17,29 @@ key_store = {} # Dictionary used for storing keys
 KEY_EXPIRY_TIME = 600 # Key expiry time set for 10 minutes
 
 
-# Function for generating a RSA Key
+# Function for generating a RSA Key pair (Private and Public)
 def generate_rsa_key():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
     
+    public_key = private_key.public_key()
+    
+    # Converting the private key to a PEM format
+    
+    private_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.NoEncryption()
+    ).decode()
+    
+    
+    # Converting the public key to a PEM format
+    public_pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    ).decode()
+    
+    
+    return private_pem, public_pem
