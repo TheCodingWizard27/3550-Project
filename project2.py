e912c4e1bf0cc071c68d346beaaa9a94a7bdc187
Raunak Upreti <raunakupretics26@gmail.com>
2025-03-23 11:54:32 -0500

    Initialized Database

--- project2.py
+++ project2.py
@@ -8,3 +8,18 @@ from fastapi.responses import JSONResponse
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 
+DB_FILE = "totally_not_my_privateKeys.db"
+app = FastAPI()
+
+# Initialize Database
+def init_db():
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS keys(
+                        kid INTEGER PRIMARY KEY AUTOINCREMENT,
+                        key TEXT NOT NULL,
+                        exp INTEGER NOT NULL)''')
+    conn.commit()
+    conn.close()
+
+init_db()
